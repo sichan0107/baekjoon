816f2291a7c42f6291e5f71f5c536aab75c7e85c
scc0103 <thdtlcks369@gmail.com>
2020-08-15 01:41:21 +0900

    알파벳 찾기

---
+++ algorithm/src/algorithm/Boj_10809.java
@@ -0,0 +1,37 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Boj_10809 {
+	public static void show(int[] result) {
+		for(int i=0; i<result.length; i++) {
+			System.out.print(result[i]);
+			System.out.print(" ");
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine(); // baekjoon);
+		sc.close();
+		int[] answer = new int[26];
+		
+		for(int i=0; i<26; i++) {
+			answer[i] = -1;
+		}
+		
+		for(int i=0; i<input.length(); i++) {
+			char a = input.charAt(i);
+			if(answer[a - 'a'] == -1) {
+				answer[a - 'a'] = i;
+			}
+			else {
+				continue;
+			}
+		}
+		show(answer);
+		
+	}
+
+}

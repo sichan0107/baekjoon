400f5adf0eb28679cb2dfe101530abeaab6bdced
scc0103 <thdtlcks369@gmail.com>
2020-08-21 01:19:25 +0900

    직각삼각형

---
+++ algorithm/src/algorithm/Boj_1085.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Boj_1085 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt(); //한수 좌표(참고 : 1보다 크거나 같고 w보다 1작거나 같아야함.)
+		int y = sc.nextInt();
+		int w = sc.nextInt(); //직사각형의 끝 좌표
+		int h = sc.nextInt();
+		
+		System.out.println(Math.min(Math.min(w-x, x), Math.min(h-y, y)));
+	}
+}

83458483834c2740c0515a98f95cf70ed74fd27c
scc0103 <thdtlcks369@gmail.com>
2020-10-14 19:57:18 +0900

    분해합

---
+++ algorithm/src/bruteForce/Boj_2231.java
@@ -0,0 +1,14 @@
+package bruteForce;
+
+import java.util.Scanner;
+
+public class Boj_2231 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); // 주어지는 분해합
+		
+		
+	}
+
+}
